c695e2f0295e5b8f899cf599c211972a3d312e6c
Dustin Nazelrod <Dustin.Nazelrod@gmail.com>
2024-02-24 00:58:50 -0500

    Delete docs directory

--- docs/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-     latestVersion: "Beta",
-   themeColor: "#53ed6d",
-   otherVersions: [ ],
-   description: "visage-documentation",
-   favicon: "assets/docsIcon.png",
-   name: "Visage" ,
-};
