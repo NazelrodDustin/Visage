2e2c5c3f687d3686524b908ee6dbb42c57700eb3
Dustin Nazelrod <Dustin.Nazelrod@gmail.com>
2024-03-08 21:21:06 -0500

    Update docs/config.js

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,8 @@
 const config = {
      favicon: "assets/docsIcon.png",
    themeColor: "#53ed6d",
+   description: "visage-documentation",
    latestVersion: "Beta",
    otherVersions: [ ],
-   name: "Visage",
-   description: "visage-documentation" ,
+   name: "Visage" ,
 };
